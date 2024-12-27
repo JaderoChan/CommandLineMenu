66d6b836415947f3b112aec8556fef5db050af29
頔珞 <c_dl_cn@outlook.com>
2024-12-28 04:56:10 +0800

    更新示例

--- example/example1.cpp
+++ example/example1.cpp
@@ -1,33 +1,79 @@
-#include <command_line_option.hpp>
+#include <command_line_menu.hpp>
 
-int main()
+int main(int argc, char* argv[])
 {
-    CommandLineOption& option = CommandLineOption::getInstance();
+    CommandLineMenu menu;
 
-    option.setMaxColumn(3);
-    option.setShowIndex(true);
-    option.setHighlightForegroundColor(0x00, 0x99, 0x33);
-    option.setHighlightBackgroundColor(-1, -1, -1); // use default color
-    option.setTopText("Welcome to the command line option test program.");
-    option.setBottomText("Use the WASD keys to navigate, and the Enter key to select an option.");
+    menu.setEnableShowIndex(true);
+    menu.setTopText("Welcome to the command line menu test program.");
+    menu.setBottomText("Use the WASD keys to navigate, and the Enter key to select an option, or the Esc key to exit.");
+    menu.setNewPageEndedText("Press ESC key to back to the main menu.");
+    menu.setMaxColumn(3);
+    menu.setEnableAutoAdjustOptionTextWidth(true);
+    menu.setOptionTextAlignment(2);
 
-    option.addOption("function A", []() {
+    menu.addOption("Function A", []() {
         std::cout << "Function A called." << std::endl;
     });
-    option.addOption("function B", []() {
+
+    menu.addOption("Function B", []() {
         std::cout << "Function B called." << std::endl;
     });
-    option.addOption("function C", nullptr);
-    option.addOption("function D", nullptr);
-    option.addOption("function E", nullptr);
-    option.addOption("function F", nullptr);
-    option.addOption("function G", nullptr);
-    option.addOption("function H", nullptr);
-    option.addOption("function I", nullptr);
-    option.addOption("function J", nullptr);
 
-    option.show();
-    option.startRecvingInput();
+    int newOptionIndex = 0;
+    using DataType1 = std::pair<int*, CommandLineMenu*>;
+    DataType1 data1(&newOptionIndex, &menu);
+    menu.addOption("Add new", [](void* data) {
+        auto data_ = static_cast<DataType1*>(data);
+
+        data_->second->addOption("Placeholder " + std::to_string((*data_->first)++), nullptr);
+    }, &data1, false);
+
+    menu.addOption("Remove last", [](void* data) {
+        auto& menu = *static_cast<CommandLineMenu*>(data);
+
+        if (menu.optionCount() > 0)
+            menu.removeOption(menu.optionCount() - 1);
+    }, &menu, false);
+
+    menu.addOption("Change column", [](void* data) {
+        auto& menu = *static_cast<CommandLineMenu*>(data);
+
+        std::cout << "Please enter the new column number: ";
+        int newColumn = 0;
+        std::cin >> newColumn;
+
+        menu.setMaxColumn(newColumn);
+    }, &menu);
+
+    menu.addOption("Sub Menu", [](void* data) {
+        auto& menu = *static_cast<CommandLineMenu*>(data);
+
+        CommandLineMenu submenu;
+        submenu.setTopText("Sub Menu");
+
+        submenu.addOption("Func 1", []() {
+            std::cout << "Hello," << std::endl;
+        });
+
+        submenu.addOption("Func 2", []() {
+            std::cout << "World!" << std::endl;
+        });
+
+        submenu.addOption("Placeholder", nullptr);
+
+        submenu.show();
+        submenu.startReceiveInput();
+    }, &menu);
+
+    menu.addOption("Exit", [](void* data) {
+        auto& menu = *static_cast<CommandLineMenu*>(data);
+
+        menu.endReceiveInput();
+    }, &menu, false);
+
+    menu.show();
+    menu.startReceiveInput();
 
     return 0;
 }
